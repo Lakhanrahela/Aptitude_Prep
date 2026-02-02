c08280f9bfa2fc90db2ac4c8f35c707930b876e0
Lakhanrahela <rahelalakhan@gmail.com>
2026-02-02 12:32:09 +0530

    Use env file for firebase

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -8,12 +8,12 @@ export const styleObj={
   margin:"40vh auto"
 }
 const firebaseConfig = {
-    apiKey: "AIzaSyCVYOY5Mqc6W7LqKF206AVDBGKgKNzjZFM",
-    authDomain: "aptitudeprep-ac201.firebaseapp.com",
-    projectId: "aptitudeprep-ac201",
-    storageBucket: "aptitudeprep-ac201.appspot.com",
-    messagingSenderId: "640929947370",
-    appId: "1:640929947370:web:6d61ced7713cd58fff8193"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
   };
 
 const app = initializeApp(firebaseConfig);
